41e80ddadcb709abca403c0a0dd2c5ced2437bc9
sabrahaim <sabrahaim@gmail.com>
2023-11-14 21:30:00 +0100

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
-    
+import numpy as np
+
 data = pd.read_csv('doppler_broad.csv')
 print(data)
 
@@ -9,10 +10,36 @@ voltage = data['voltage']
 time = data['time']
 print(voltage, time)
 
+plt.figure('plot')
 plt.plot(time, voltage)
 plt.xlabel('time (ms)')
 plt.ylabel('voltage (V)')
 plt.show()
+
+width_time = data['time'][2499] - data['time'][0]
+print(width_time)
+# width = 0.099959999328 ms
+
+width_frequency = 6834,683 
+# width = 6834,683*10**6 hz
+
+df = 6834.683/len(time)
+print(df)
+
+freq = []
+for punten in range(len(time)):
+    data_punten = punten*df 
+    freq.append(data_punten)
+        
+print(freq)
+
+
+
+
+
+
+
+
 
 
 
